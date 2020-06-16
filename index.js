3cf295dbb4558ba2d6ce566ed4226db39aa72e0b
awesh985 <66862444+awesh985@users.noreply.github.com>
2020-06-16 18:26:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 const express = require('express');
 const app = express();
 app.use(express.json());
-const ROUTER = require('./route/routes');
+const ROUTER = require('./routes');
+//mongodb+srv://abesh:abesh@cluster0-duuve.mongodb.net/registration?retryWrites=true&w=majority
+mongoose.connect("mongodb+srv://abesh:abesh@cluster0-duuve.mongodb.net/registration?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true })
 
 
 
-app.use('/',ROUTER)
+
+
+app.use('/',ROUTER);
 const port =process.env.AWESH || 3000;
 app.listen(port,() => console.log('linstening on port ${AWESH}...')) ;
